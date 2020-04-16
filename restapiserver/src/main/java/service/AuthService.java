2873780a5c5ea62c1d732c6f214e9cb799402a89
sangminLee <nalsm98@naver.com>
2020-04-16 17:24:06 +0900

    회원 인증 서비스 구현

---
+++ restapiserver/src/main/java/service/AuthService.java
@@ -0,0 +1,52 @@
+package service;
+
+import dto.Member;
+import error.DuplicateEmailException;
+import error.MemberNotFoundException;
+import error.UnauthorizedException;
+import error.WrongPasswordException;
+import mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpSession;
+
+public class AuthService {
+
+  @Autowired
+  private MemberMapper memberMapper;
+
+  public Member login(String email, String password) {
+    Member member = memberMapper.selectOneByEmail(email);
+    if (member == null)
+      throw new MemberNotFoundException();
+    if (!member.matchPassword(password))
+      throw new WrongPasswordException();
+    return member;
+  }
+
+  public void signUp(String name, String email, String password) {
+    Member member = memberMapper.selectOneByEmail(email);
+    if (member != null)
+      throw new DuplicateEmailException();
+    memberMapper.insert(name, email, password);
+  }
+
+  public Member getInfo(int id, HttpSession session) {
+    Member member = memberMapper.selectOneById(id);
+    if (member == null)
+      throw new MemberNotFoundException();
+    if (session.getAttribute(member.getEmail()) == null)
+      throw new UnauthorizedException();
+    return member;
+  }
+
+  public void update(String name, String password, String email, HttpSession session) {
+    Member member = memberMapper.selectOneByEmail(email);
+    if (member == null)
+      throw new MemberNotFoundException();
+    if (session.getAttribute(member.getEmail()) == null)
+      throw new UnauthorizedException();
+    memberMapper.update(name, password, email);
+  }
+
+}
