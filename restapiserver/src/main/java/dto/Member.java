1e7d3711e7babf0f3714e2fe06ba81f4e2564c2f
sangminLee <nalsm98@naver.com>
2020-04-16 17:22:50 +0900

    회원 DTO

---
+++ restapiserver/src/main/java/dto/Member.java
@@ -0,0 +1,45 @@
+package dto;
+
+public class Member {
+
+  private int id;
+  private String name;
+  private String email;
+  private String password;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean matchPassword(String password) {
+    return this.password.equals(password);
+  }
+}
