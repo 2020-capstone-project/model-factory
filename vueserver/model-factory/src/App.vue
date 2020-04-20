2297facae7174c3bbc16a783c7a3bede4eef5e12
sangminLee <nalsm98@naver.com>
2020-04-20 22:07:58 +0900

    헤더, 네비게이션, 콘텐츠 분리 완료

--- vueserver/model-factory/src/App.vue
+++ vueserver/model-factory/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <HomeNavigation></HomeNavigation>
+
+    <AppHeader></AppHeader>
+
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import HomeNavigation from '@/components/common/HomeNavigation.vue';
+import AppHeader from '@/components/common/AppHeader.vue';
+
 export default {
   name: 'App',
+  components: {
+    HomeNavigation,
+    AppHeader,
+  },
 };
 </script>
