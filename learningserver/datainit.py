4d119c4fcac7178cac680f0f678a214e0909c572
Sangmin Lee <nalsm98@naver.com>
2020-06-03 00:59:17 +0900

    데이터 전처리 구현완료

--- learningserver/datainit.py
+++ learningserver/datainit.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 
 data = {
     "filepath": "/Users/sangminlee/model-factory/restapiserver/src/main/resources/홍릉_도심_미세먼지_측정자료.csv",
@@ -33,28 +35,35 @@ data = {
     ]
 }
 
-# dataset = np.loadtxt(data['filepath'], delimiter=',')
+# 파일 불러오기
 dataset = pd.read_csv(data['filepath'])
-inputColumns = []
-for col in data.get('inputColumns'):
-    inputColumns.append(dataset[col])
-    print('\n=== column ===\n')
-    print(col)
-    print()
 
-print('\n=== dataset ===\n')
-print(dataset)
+# 입력 데이터 전처리 및 학습셋, 검증셋, 시험셋 분리
+x_train = dataset[data.get('inputColumns')].values[1:].astype('float32')
+train_length = int(len(x_train) * 0.7)
+val_length = int(len(x_train) * 0.2)
+x_test = x_train[train_length + val_length:]
+x_val = x_train[train_length: train_length + val_length]
+x_train = x_train[:train_length]
 
-# 배열 하나로 합치기
-#   np.concatenate((a, b), axis=?))
 
-# pandas to numpy
-#   df.to_numpy()
+# 결과 데이터 전처리 및 학습셋, 검증셋, 시험셋 분리
+y_train = dataset[data.get('outputColumns')].values[1:].astype('float32')
+y_test = y_train[train_length + val_length:]
+y_val = y_train[train_length: train_length + val_length]
+y_train = y_train[:train_length]
+print(len(y_test[0]))
 
-x_train = []
+x_length = len(x_test[0])
+y_length = len(y_test[0])
 
-print('\n=== inputColumns ===\n')
-for col in inputColumns:
-    print('\n===', col[0], '===\n')
-    inputData = [value for value in col]
-    x_train.append(inputData)
+model = Sequential()
+model.add(Dense(256, input_dim=len(x_test[0]), activation="relu"))
+model.add(Dense(256, activation="relu"))
+model.add(Dense(256, activation="relu"))
+model.add(Dense(1))
+
+model.compile(optimizer=data.get('optimizerFunction'), loss=data.get('lossFunction'),
+              metrics=['accuracy'])
+
+hist = model.fit(x_train, y_train, epochs=200, batch_size=64)
