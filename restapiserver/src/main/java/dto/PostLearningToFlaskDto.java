08c7ffc8b519655d334d374046e672f6263b7f64
Sangmin Lee <nalsm98@naver.com>
2020-06-06 23:21:40 +0900

    Spring & Flask 통신에 필요한 Dto 추가

---
+++ restapiserver/src/main/java/dto/PostLearningToFlaskDto.java
@@ -0,0 +1,42 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostLearningToFlaskDto {
+
+  private String learningFilePath;
+  private String modelPath;
+  private String diagramPath;
+  private String prediction;
+  private List<String> inputColumns;
+  private List<String> outputColumns;
+  private int batchSize;
+  private int epoch;
+  private String lossFunction;
+  private String optimizerFunction;
+  private List<LayerDto> layers;
+
+  public PostLearningToFlaskDto(String learningFilePath, ModelDto modelDto, PostLearningRequestDto dto) {
+    this.learningFilePath = learningFilePath;
+    modelPath = modelDto.getModelPath();
+    diagramPath = modelDto.getDiagramPath();
+    prediction = dto.getPrediction();
+    inputColumns = dto.getInputColumns();
+    outputColumns = dto.getOutputColumns();
+    batchSize = dto.getBatchSize();
+    epoch = dto.getEpoch();
+    lossFunction = dto.getLossFunction();
+    optimizerFunction = dto.getOptimizerFunction();
+    layers = dto.getLayers();
+  }
+
+}
