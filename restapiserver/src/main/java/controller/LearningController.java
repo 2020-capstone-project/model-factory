fba2f6e08881074cc599181a521a6732f94950b4
Sangmin Lee <nalsm98@naver.com>
2020-06-06 23:20:25 +0900

    Spring & Flask 통신 구현

--- restapiserver/src/main/java/controller/LearningController.java
+++ restapiserver/src/main/java/controller/LearningController.java
@@ -1,10 +1,9 @@
 package controller;
 
-import dto.LearningDto;
-import dto.ModelDto;
-import dto.PostLearningRequestDto;
-import dto.ResultByInsertLearningDto;
+import client.ClientApi;
+import dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,15 +25,20 @@ public class LearningController {
   private LayerService layerService;
   @Autowired
   private FileService fileService;
+  @Autowired
+  private ClientApi clientApi;
 
   @PostMapping("/learning")
+  // @Transactional
   public ResultByInsertLearningDto postLearning(@RequestBody @Valid PostLearningRequestDto dto) {
     ResultByInsertLearningDto resultByInsertLearningDto
         = learningService.insert(new LearningDto(dto));
     ModelDto modelDto = new ModelDto(resultByInsertLearningDto);
     modelService.insert(modelDto);
     layerService.insert(dto.getLayers(), modelDto.getId());
-    // TODO Flask 로 데이터 전송 필요
+    clientApi.postLearningToFlask(
+        new PostLearningToFlaskDto(
+            fileService.selectPathById(dto.getFileId()), modelDto, dto));
     return resultByInsertLearningDto;
   }
 
