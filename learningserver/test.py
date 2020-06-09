711cf1cb7113fc76007e3220f19d8f9c4abe1d4c
Sangmin Lee <nalsm98@naver.com>
2020-06-09 23:20:05 +0900

    Spring & Flask 통신 테스트

--- learningserver/test.py
+++ learningserver/test.py
@@ -2,12 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/learning', methods=['POST'])
 def post_learning():
     learning = request.get_json()
     print(learning)
-    # TODO 반환 값 수정 (문자열 X, JSON O)
-    return {"test" : "1234"}
+    return {"test": "1234"}
+
 
 if __name__ == '__main__':
     app.run()
