b76674412b5058a285f2a07fd3f6d7f70a8a9b02
Sangmin Lee <nalsm98@naver.com>
2020-06-21 19:04:01 +0900

    Learning Client Service 필수 로직 구현 완료
    * 모델을 저장하는 로직 구현
    * API Server에 POST 메서드로 히스토리 정보를 저장 요청하는 로직 구현
    * DTO로부터 모델 경로를 전달하는 코드 추가
    * TODO 리스트 변경

--- learningserver/implement/service/ClientService.py
+++ learningserver/implement/service/ClientService.py
@@ -1,25 +1,27 @@
 import requests, json
 from threading import Thread
 import time
-
+from keras.models import load_model
+import os
 
 class ClientService:
     def __init__(self):
         self.URL = 'http://localhost:8080/'
 
-    # TODO Model 저장 로직 구현
+    # TODO 테스트셋으로 검증후 저장 요청 로직 구현
+    # TODO 예외 처리
 
     def saveHistory(self, learningId, epochCount, trainLoss, trainAccuracy,
                     valLoss, valAccuracy, model, modelPath):
-        # TODO Spring 서버와 통신이 잘 되는지 확인 필요
         url = self.URL + 'history'
+        model.save(modelPath)
         data = {'learningId': learningId,
                 'epochCount': epochCount,
                 'trainLoss': trainLoss,
                 'trainAccuracy': trainAccuracy,
                 'valLoss': valLoss,
                 'valAccuracy': valAccuracy}
-        # requests.post(url, data=json.dumps(data))
+        print(requests.post(url, json=data))
 
     def requestSaveHistory(self, historyDto):
         Thread(target=self.saveHistory, args=(historyDto.learningId,
@@ -28,4 +30,5 @@ class ClientService:
                                               historyDto.trainAccuracy,
                                               historyDto.valLoss,
                                               historyDto.valAccuracy,
-                                              historyDto.model)).start()
+                                              historyDto.model,
+                                              historyDto.modelPath)).start()
