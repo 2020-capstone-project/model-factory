36eb3109626282fd7a3e81394de52c1da7dae6c9
Sangmin Lee <nalsm98@naver.com>
2020-07-27 01:25:31 +0900

    히스토리 매퍼 수정
    * validationLoss, vadlidationAccuracy 로 파라미터 수정

--- restapiserver/src/main/java/mapper/HistoryMapper.java
+++ restapiserver/src/main/java/mapper/HistoryMapper.java
@@ -2,12 +2,11 @@ package mapper;
 
 import dto.PostHistoryRequestDto;
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
 
 public interface HistoryMapper {
 
-  @Insert("insert into history (trainLoss, trainAccuracy, verificationLoss, " +
-      "verificationAccuracy, createdDate, executedEpoch, learningId) values " +
+  @Insert("insert into history (trainLoss, trainAccuracy, validationLoss, " +
+      "validationAccuracy, createdDate, executedEpoch, learningId) values " +
       "(#{trainLoss}, #{trainAccuracy}, #{valLoss}, " +
       "#{valAccuracy}, now(), #{epochCount}, #{learningId})")
   void insert(PostHistoryRequestDto postHistoryRequestDto);
