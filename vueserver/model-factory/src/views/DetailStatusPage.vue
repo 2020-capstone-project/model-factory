684b4aaff27cb96b3f4befdc9558cf65229f3541
Sangmin Lee <nalsm98@naver.com>
2020-07-27 01:21:32 +0900

    학습 현황 페이지 구현중

---
+++ vueserver/model-factory/src/views/DetailStatusPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="3">
+        <v-alert border="left" colored-border color="blue" elevation="2">
+          <div class="ml-4">
+            <h3 class="blue--text mb-1">학습명</h3>
+            <h2>당뇨병 발병 여부 예측 모델</h2>
+          </div>
+        </v-alert>
+      </v-col>
+      <v-col cols="3">
+        <v-alert border="left" colored-border color="indigo" elevation="2">
+          <div class="ml-4">
+            <h3 class="indigo--text mb-1">학습 시작 날짜</h3>
+            <h2>2020.01.01 00:00</h2>
+          </div>
+        </v-alert>
+      </v-col>
+      <v-col cols="3">
+        <v-alert border="left" colored-border color="teal" elevation="2">
+          <div class="ml-4">
+            <h3 class="teal--text mb-1">학습 진행률</h3>
+            <v-progress-linear
+              color="teal"
+              :value="learningProgress"
+              height="35"
+              rounded
+            >
+              <strong class="white--text">70%</strong>
+            </v-progress-linear>
+          </div>
+        </v-alert>
+      </v-col>
+      <v-col cols="3">
+        <v-alert border="left" colored-border color="purple" elevation="2">
+          <div class="ml-4">
+            <h3 class="purple--text mb-1">학습 정확도</h3>
+            <v-progress-linear color="purple" :value="accuracy" height="35">
+              <strong class="white--text">78%</strong>
+            </v-progress-linear>
+          </div>
+        </v-alert>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-card elevation="10" class="primary" primary dark>
+          <v-card-title>
+            Learning Graph
+          </v-card-title>
+          <v-card-text class="white text-primary">
+            asd
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    learningProgress: 70,
+    accuracy: 78,
+  }),
+};
+</script>
+
+<style></style>
--- vueserver/model-factory/src/views/LearningStatusPage.vue
+++ vueserver/model-factory/src/views/LearningStatusPage.vue
@@ -1,9 +1,87 @@
 <template>
-  <div>learning status</div>
+  <v-container fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="10">
+        <v-card>
+          <v-data-table
+            :headers="headers"
+            :items="statusList"
+            :search="search"
+            sort-by="calories"
+            class="elevation-1"
+          >
+            <template v-slot:top>
+              <v-toolbar flat color="primary">
+                <v-toolbar-title class="text-white text-h5"
+                  >Learning Status</v-toolbar-title
+                >
+              </v-toolbar>
+              <v-text-field
+                v-model="search"
+                label="찾으실 학습명을 입력해주세요."
+                class="mx-4"
+              ></v-text-field>
+            </template>
+            <template v-slot:item.tools="{ item }">
+              <v-icon large class="ml-3" @click="editItem(item)">
+                mdi-note-outline
+              </v-icon>
+            </template>
+            <template v-slot:no-data>
+              <v-btn color="primary" @click="fetchData">Reset</v-btn>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import { getList } from '@/api/learningStatus';
+
+export default {
+  data: () => ({
+    search: '',
+    dialog: false,
+    headers: [
+      {
+        text: '학습명',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: '학습 시작 날짜', value: 'learningDate' },
+      { text: '학습 상태', value: 'learningState' },
+      { text: '학습 진행률(%)', value: 'learningProgress' },
+      { text: '모델 정확도(%)', value: 'accuracy' },
+      { text: '자세히 보기', value: 'tools', sortable: false },
+    ],
+    statusList: [],
+  }),
+
+  computed: {},
+
+  created() {
+    this.fetchData();
+  },
+
+  methods: {
+    async fetchData() {
+      try {
+        const { data } = await getList(this.$store.getters.getMemberId);
+        this.statusList = data;
+      } catch (error) {
+        // TODO 예외 처리: 메시지로 표시
+      }
+    },
+    editItem(item) {
+      this.editedIndex = this.statusList.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+  },
+};
 </script>
 
 <style></style>
