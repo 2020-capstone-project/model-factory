202a5fee9ff53a40a7ef6c10c4d509225aa6a2fb
Sangmin Lee <nalsm98@naver.com>
2020-07-27 01:26:57 +0900

    학습 매퍼 수정
    * name(학습명)을 저장할 수 있도록 수정

--- restapiserver/src/main/java/mapper/LearningMapper.java
+++ restapiserver/src/main/java/mapper/LearningMapper.java
@@ -6,8 +6,8 @@ import org.apache.ibatis.annotations.*;
 public interface LearningMapper {
 
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  @Insert("insert into learning (batchSize, epoch, lossFunction, optimizerFunction, learningDate, memberId) " +
-      "values (#{batchSize}, #{epoch}, #{lossFunction}, #{optimizerFunction}, now(), #{memberId})")
+  @Insert("insert into learning (batchSize, epoch, lossFunction, optimizerFunction, learningDate, memberId, name) " +
+      "values (#{batchSize}, #{epoch}, #{lossFunction}, #{optimizerFunction}, now(), #{memberId}, #{name})")
   void insert(LearningDto learningDto);
 
 }
