a94a7a864b8b550d69d612a97afaa4fbc9a49ace
Sangmin Lee <nalsm98@naver.com>
2020-07-27 01:21:15 +0900

    학습 페이지 구현 완료

--- vueserver/model-factory/src/views/LearningPage.vue
+++ vueserver/model-factory/src/views/LearningPage.vue
@@ -84,6 +84,23 @@
         </v-card-text>
       </v-card>
     </v-footer>
+    <v-dialog
+      v-model="$store.state.learningDialog"
+      hide-overlay
+      persistent
+      width="300"
+    >
+      <v-card color="primary" dark>
+        <v-card-text>
+          잠시만 기다려주세요
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -109,7 +126,9 @@ export default {
     ModelCustomizeContent,
   },
   data() {
-    return {};
+    return {
+      dialog: true,
+    };
   },
   computed: {
     getIsSelectModel() {
