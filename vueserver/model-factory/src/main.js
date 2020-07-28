1fb73777879f6b06c8251c029ca8bd468d7d68eb
Sangmin Lee <nalsm98@naver.com>
2020-07-28 19:27:10 +0900

    메인 js 수정
    * highcharts-vue 모듈 추가

--- vueserver/model-factory/src/main.js
+++ vueserver/model-factory/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import HighchartsVue from 'highcharts-vue';
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  HighchartsVue,
   render: h => h(App),
 }).$mount('#app');
