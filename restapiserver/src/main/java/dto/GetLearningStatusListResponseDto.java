9418f61a6e024c284e7c2ca4c01e3ca94a2693d8
Sangmin Lee <nalsm98@naver.com>
2020-07-28 18:04:57 +0900

    학습 현황 리스트 조회 DTO 수정
    * 학습 시작 시간을 Default 에서 서울 시간으로 변경

--- restapiserver/src/main/java/dto/GetLearningStatusListResponseDto.java
+++ restapiserver/src/main/java/dto/GetLearningStatusListResponseDto.java
@@ -16,10 +16,22 @@ public class  GetLearningStatusListResponseDto {
 
   private int id;
   private String name;
-  @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
   private Timestamp learningDate;
   private String learningState;
   private int learningProgress;
   private double accuracy;
 
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("GetLearningStatusListResponseDto{");
+    sb.append("id=").append(id);
+    sb.append(", name='").append(name).append('\'');
+    sb.append(", learningDate=").append(learningDate);
+    sb.append(", learningState='").append(learningState).append('\'');
+    sb.append(", learningProgress=").append(learningProgress);
+    sb.append(", accuracy=").append(accuracy);
+    sb.append('}');
+    return sb.toString();
+  }
 }
