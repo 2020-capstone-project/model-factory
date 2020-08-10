bfe98b66a13d0c579cc50dc636d1fad7321e004e
Sangmin Lee <nalsm98@naver.com>
2020-08-10 21:18:13 +0900

    파일 업로드 로직 구현중

--- restapiserver/src/main/java/controller/FileController.java
+++ restapiserver/src/main/java/controller/FileController.java
@@ -3,7 +3,10 @@ package controller;
 import dto.FileDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import service.FileService;
 
 import java.util.List;
@@ -17,6 +20,11 @@ public class FileController {
   @GetMapping("/files")
   public List<FileDto> getFiles() {
     return service.selectList();
+  }
+
+  @PostMapping("/upload")
+  public void upload(@RequestParam("file") MultipartFile multipartFile) {
+
   }
 
 }
--- restapiserver/src/main/java/service/FileService.java
+++ restapiserver/src/main/java/service/FileService.java
@@ -5,8 +5,15 @@ import error.FileNotFoundException;
 import mapper.FileMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class FileService {
@@ -23,6 +30,27 @@ public class FileService {
     if (path == null)
       throw new FileNotFoundException();
     return path;
+  }
+
+  public void upload(MultipartFile multipartFile) {
+    File targetFile = new File("/Users/sangminlee/model-factory/restapiserver/src/main/resources/files/" +
+        multipartFile.getName());
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(targetFile));
+      String line = "";
+      while ((line = br.readLine()) != null) {
+        System.out.println("1");
+        System.out.println(Arrays.toString(line.split(",", -1)));
+      }
+      while ((line = br.readLine()) != null) {
+        System.out.println("1");
+        System.out.println(Arrays.toString(line.split(",", -1)));
+      }
+    } catch (java.io.FileNotFoundException e) {
+      throw new FileNotFoundException();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
