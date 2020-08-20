700e98978add6b206bb93a87a346d0098cdc7cc4
Sangmin Lee <nalsm98@naver.com>
2020-08-20 18:38:18 +0900

    파일 처리를 위한 설정을 해주는 빈 추가

--- restapiserver/src/main/java/config/RestConfig.java
+++ restapiserver/src/main/java/config/RestConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"controller", "service"})
 public class RestConfig {
+
+  private final int MAX_SIZE = 10 * 1024 * 1024;
 
   @Bean
   public MessageSource messageSource() {
@@ -19,6 +23,15 @@ public class RestConfig {
     ms.setBasenames("message.label");
     ms.setDefaultEncoding("UTF-8");
     return ms;
+  }
+
+  @Bean(name = "multipartResolver")
+  public MultipartResolver commonsMultipartResolver() {
+    CommonsMultipartResolver cmr = new CommonsMultipartResolver();
+    cmr.setMaxUploadSize(MAX_SIZE);
+    cmr.setMaxUploadSizePerFile(MAX_SIZE);
+    cmr.setMaxInMemorySize(0);
+    return cmr;
   }
 
 }
