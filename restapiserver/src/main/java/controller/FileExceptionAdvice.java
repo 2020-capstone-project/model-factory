149a7c20541e4b16ae0b6bfda3101c5bbdd6b45b
Sangmin Lee <nalsm98@naver.com>
2020-08-20 18:37:13 +0900

    파일 업로드 예외 처리 구현 완료

--- restapiserver/src/main/java/controller/FileExceptionAdvice.java
+++ restapiserver/src/main/java/controller/FileExceptionAdvice.java
@@ -1,20 +1,38 @@
 package controller;
 
+import error.ColumnDescriptionIsNumericException;
 import error.ErrorResponse;
 import error.FileNotFoundException;
+import error.NotExistValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
+
 @RestControllerAdvice
 public class FileExceptionAdvice {
 
-  @ExceptionHandler(FileNotFoundException.class)
+  @ExceptionHandler({FileNotFoundException.class, IOException.class})
   public ResponseEntity<ErrorResponse> fileNotFound() {
     return ResponseEntity
         .status(HttpStatus.NOT_FOUND)
         .body(new ErrorResponse("파일을 찾을 수 없습니다."));
+  }
+
+  @ExceptionHandler(ColumnDescriptionIsNumericException.class)
+  public ResponseEntity<ErrorResponse> columnDescriptionIsNumeric() {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse("파일의 컬럼 설명은 문자로 이루어져야 합니다."));
+  }
+
+  @ExceptionHandler(NotExistValueException.class)
+  public ResponseEntity<ErrorResponse> notExistValue() {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse("파일에 빈 칸이 존재합니다. 파일의 모든 값은 존재해야 합니다."));
   }
 
 }
