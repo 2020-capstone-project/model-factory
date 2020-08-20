01001b3b78c9c9b78ed8c9b46310a002f4a0e88f
Sangmin Lee <nalsm98@naver.com>
2020-08-20 18:39:44 +0900

    파일 형식 점검, 파일 저장 유틸리티 구현중

---
+++ restapiserver/src/main/java/util/FileManagementUtil.java
@@ -0,0 +1,7 @@
+package util;
+
+public class FileManagementUtil {
+
+  // TODO 파일 저장 및 삭제 기능 구현
+
+}
---
+++ restapiserver/src/main/java/util/FileValidCheckUtil.java
@@ -0,0 +1,83 @@
+package util;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import error.ColumnDescriptionIsNumericException;
+import error.NotExistValueException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FileValidCheckUtil {
+
+  private final String DIRECTORY_PATH = "/Users/sangminlee/model-factory/restapiserver/src/main/resources/files/";
+
+  private FileValidCheckUtil() {}
+
+  public static FileValidCheckUtil fileValidCheckUtil = new FileValidCheckUtil();
+
+  public static FileValidCheckUtil getInstance() {
+    return fileValidCheckUtil;
+  }
+
+  public boolean validFile(MultipartFile multipartFile) throws IOException {
+    return validRecords(getRecords(multipartFile));
+  }
+
+  private List<List<String>> getRecords(MultipartFile multipartFile) throws IOException {
+    List<List<String>> records = new ArrayList<>();
+    try (CSVReader csvReader = new CSVReader(getFileReader(multipartFile.getName()))) {
+      String[] values;
+      while ((values = csvReader.readNext()) != null) {
+        records.add(Arrays.asList(values));
+      }
+    } catch (IOException | CsvValidationException e) {
+      throw new IOException();
+    }
+    return records;
+  }
+
+  private FileReader getFileReader(String fileName) throws FileNotFoundException {
+    return new FileReader(DIRECTORY_PATH + fileName + ".csv");
+  }
+
+  public boolean validRecords(List<List<String>> records) {
+    if (!columnDescriptionIsNotNumeric(records))
+      throw new ColumnDescriptionIsNumericException();
+    records.remove(0);
+    records.remove(0);
+    if (!existAllValue(records))
+      throw new NotExistValueException();
+    return true;
+  }
+
+  private boolean isNotNumeric(String s) {
+    try {
+      Double.parseDouble(s);
+      return false;
+    } catch (NumberFormatException e) {
+      return true;
+    }
+  }
+
+  private boolean existAllValue(List<List<String>> records) {
+    return records
+        .parallelStream()
+        .flatMap(Collection::stream)
+        .allMatch(str -> !str.isBlank() && !isNotNumeric(str));
+  }
+
+  private boolean columnDescriptionIsNotNumeric(List<List<String>> records) {
+    return Stream.of(records.get(0), records.get(1))
+        .flatMap(Collection::stream)
+        .allMatch(str -> isNotNumeric(str) && !str.isBlank());
+  }
+
+}
