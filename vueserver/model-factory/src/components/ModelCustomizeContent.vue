9d5cbb03b2f272ed85d5655d7c24512db77876b6
Sangmin Lee <nalsm98@naver.com>
2020-08-31 12:53:08 +0900

    모델 커스터마이징 UI 구현 완료

--- vueserver/model-factory/src/components/ModelCustomizeContent.vue
+++ vueserver/model-factory/src/components/ModelCustomizeContent.vue
@@ -1,9 +1,79 @@
 <template>
-  <div>customize</div>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col>
+        <v-card elevation="10" class="primary" primary dark>
+          <v-card-title>
+            Deep Learning
+          </v-card-title>
+          <v-card-text class="white text-primary">
+            <v-row align="center">
+              <v-col cols="4">
+                <v-img src="../../public/img/deeplearning2.png" />
+              </v-col>
+              <v-col cols="8">
+                <v-tabs v-model="tab" light>
+                  <v-tab v-for="item in items" :key="item.tab">
+                    {{ item.tab }}
+                  </v-tab>
+                </v-tabs>
+                <v-tabs-items v-model="tab">
+                  <v-tab-item>
+                    <v-card flat>
+                      <HyperparametersCustomizeContent></HyperparametersCustomizeContent>
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <LayersCustomizeContent></LayersCustomizeContent>
+                  </v-tab-item>
+                </v-tabs-items>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" class="mt-5">
+      <v-btn color="grey text-white" @click="before">
+        before
+      </v-btn>
+      <v-col col="1"></v-col>
+      <v-btn color="primary" @click="next" class="ml-5">
+        Continue
+      </v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import HyperparametersCustomizeContent from './HyperparametersCustomizeContent.vue';
+import LayersCustomizeContent from './LayersCustomizeContent.vue';
+
+export default {
+  components: {
+    HyperparametersCustomizeContent,
+    LayersCustomizeContent,
+  },
+  data() {
+    return {
+      tab: 0,
+      items: [
+        {
+          tab: '하이퍼 파라미터',
+        },
+        { tab: '레이어' },
+      ],
+    };
+  },
+  methods: {
+    before() {
+      this.$store.commit('beforeSequence');
+    },
+    next() {
+      this.$store.commit('nextSequence');
+    },
+  },
+};
 </script>
 
 <style></style>
