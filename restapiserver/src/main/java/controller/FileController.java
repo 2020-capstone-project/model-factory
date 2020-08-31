48c2e43db1ce30f2255500333813a071670954aa
Sangmin Lee <nalsm98@naver.com>
2020-08-31 12:57:18 +0900

    파일 업로드 및 조회 처리 수정 및 생성

--- restapiserver/src/main/java/controller/FileController.java
+++ restapiserver/src/main/java/controller/FileController.java
@@ -1,32 +1,51 @@
 package controller;
 
+import dto.ColumnAndDataDto;
 import dto.FileDto;
+import dto.PostUploadDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import service.ColumnService;
+import service.DataService;
 import service.FileService;
-import util.FileValidCheckUtil;
+import service.MemberService;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class FileController {
 
   @Autowired
-  private FileService service;
+  private FileService fileService;
+  @Autowired
+  private MemberService memberService;
+  @Autowired
+  private ColumnService columnService;
+  @Autowired
+  private DataService dataService;
 
-  @GetMapping("/files")
-  public List<FileDto> getFiles() {
-    return service.selectList();
+  @GetMapping("/files/{id}")
+  public List<FileDto> getFiles(@PathVariable int id) {
+    return fileService.selectListByMemberId(id);
   }
 
   @PostMapping("/upload")
-  public void upload(@RequestParam MultipartFile file) throws IOException {
-    service.upload(file);
+  @Transactional
+  public PostUploadDto upload(@RequestParam("file") MultipartFile file,
+                              @RequestParam String email,
+                              @RequestParam boolean isPublic,
+                              @RequestParam String description) throws IOException {
+    fileService.selectByName(Objects.requireNonNull(file.getOriginalFilename()).replace(".csv", ""));
+    List<List<String>> records = fileService.saveFileToStorage(file);
+    int memberId = memberService.selectIdByEmail(email);
+    int fileId = fileService.insertFileToDatabase(file, description, isPublic, memberId);
+    List<ColumnAndDataDto> columnAndDataList = columnService.insertColumns(records, fileId);
+    dataService.insertData(columnAndDataList);
+    return new PostUploadDto(fileId);
   }
 
 }
