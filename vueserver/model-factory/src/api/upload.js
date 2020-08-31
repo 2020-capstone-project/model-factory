326268b70f0b73708df12bc2f6b134e4ad818d29
Sangmin Lee <nalsm98@naver.com>
2020-08-31 12:51:28 +0900

    log 제거

--- vueserver/model-factory/src/api/upload.js
+++ vueserver/model-factory/src/api/upload.js
@@ -1,7 +1,6 @@
 import { upload } from './index';
 
 function uploadFile(file) {
-  console.log(file);
   return upload.post('', file, {
     headers: {
       'Content-Type': `multipart/form-data; boundary=${file.size}; application/json`,
