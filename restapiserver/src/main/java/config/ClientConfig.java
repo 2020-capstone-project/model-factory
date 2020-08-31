d778e54903346b91f9c1e3e5368342a1b95c58be
Sangmin Lee <nalsm98@naver.com>
2020-08-31 12:55:07 +0900

    불필요한 import 제거

--- restapiserver/src/main/java/config/ClientConfig.java
+++ restapiserver/src/main/java/config/ClientConfig.java
@@ -1,12 +1,8 @@
 package config;
 
 import client.ClientApi;
-import jdk.internal.net.http.HttpClientBuilderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-import sun.net.www.http.HttpClient;
 
 @Configuration
 public class ClientConfig {
