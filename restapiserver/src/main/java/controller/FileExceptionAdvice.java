666c39fb4a9a8e7152fe0caa273b960448f57b0b
Sangmin Lee <nalsm98@naver.com>
2020-08-31 12:47:46 +0900

    파일 업로드 관련 예외 처리 수정 및 추가

--- restapiserver/src/main/java/controller/FileExceptionAdvice.java
+++ restapiserver/src/main/java/controller/FileExceptionAdvice.java
@@ -1,9 +1,6 @@
 package controller;
 
-import error.ColumnDescriptionIsNumericException;
-import error.ErrorResponse;
-import error.FileNotFoundException;
-import error.NotExistValueException;
+import error.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,18 +18,25 @@ public class FileExceptionAdvice {
         .body(new ErrorResponse("파일을 찾을 수 없습니다."));
   }
 
-  @ExceptionHandler(ColumnDescriptionIsNumericException.class)
+  @ExceptionHandler({ColumnDescriptionIsNumericException.class, NotExistValueException.class})
   public ResponseEntity<ErrorResponse> columnDescriptionIsNumeric() {
     return ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
-        .body(new ErrorResponse("파일의 컬럼 설명은 문자로 이루어져야 합니다."));
+        .body(new ErrorResponse("파일의 형식이 잘못되었습니다. 파일을 점검해주세요."));
   }
 
-  @ExceptionHandler(NotExistValueException.class)
-  public ResponseEntity<ErrorResponse> notExistValue() {
+  @ExceptionHandler({MinExistValueSizeException.class})
+  public ResponseEntity<ErrorResponse> minExistValueSize() {
     return ResponseEntity
         .status(HttpStatus.BAD_REQUEST)
-        .body(new ErrorResponse("파일에 빈 칸이 존재합니다. 파일의 모든 값은 존재해야 합니다."));
+        .body(new ErrorResponse("파일의 데이터 개수는 최소 100개 이상입니다."));
+  }
+
+  @ExceptionHandler({SameFileNameException.class})
+  public ResponseEntity<ErrorResponse> sameFileName() {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse("동일한 이름의 파일이 이미 존재합니다."));
   }
 
 }
