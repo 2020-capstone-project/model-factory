4c0d2e4e2bd43e50aecfc9302cbfb95341729717
Sangmin Lee <nalsm98@naver.com>
2020-09-05 00:04:20 +0900

    학습 컨트롤러 업데이트

--- restapiserver/src/main/java/controller/LearningController.java
+++ restapiserver/src/main/java/controller/LearningController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import client.ClientApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,7 @@ import service.LearningService;
 import service.ModelService;
 
 import javax.validation.Valid;
+import java.sql.SQLException;
 
 @RestController
 public class LearningController {
@@ -32,9 +34,10 @@ public class LearningController {
   @Transactional
   // TODO 에러 처리
   // TODO 서비스 분할을 해야 되는지 고민
-  public ResultByInsertLearningDto postLearning(@RequestBody @Valid PostLearningRequestDto dto) {
+  public ResultByInsertLearningDto postLearning(@RequestBody @Valid PostLearningRequestDto dto)
+      throws JsonProcessingException, SQLException {
     ResultByInsertLearningDto resultByInsertLearningDto
-        = learningService.insert(new LearningDto(dto));
+        = learningService.insert(dto);
     ModelDto modelDto = new ModelDto(resultByInsertLearningDto);
     modelService.insert(modelDto);
     layerService.insert(dto.getLayers(), modelDto.getId());
