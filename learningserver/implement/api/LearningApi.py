0b13a7b7cd8bcdf7e566882827a5b7f78dbeb5c1
Sangmin Lee <nalsm98@naver.com>
2020-09-04 23:54:48 +0900

    모델 테스트 API 구현 완료

--- learningserver/implement/api/LearningApi.py
+++ learningserver/implement/api/LearningApi.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 
 from implement.service.LearningService import LearningService
+from implement.service.TestService import TestService
 
 app = Flask(__name__)
 
@@ -10,6 +11,13 @@ def postLearning():
     learningService = LearningService(request.get_json())
     response = learningService.requestLearning()
     return make_response(jsonify({"message": response['message']}), response['code'])
+
+
+@app.route('/test', methods=['POST'])
+def postTest():
+    service = TestService(request.get_json())
+    result = service.testModel()
+    return make_response(jsonify({'result': result}), 200)
 
 
 if __name__ == '__main__':
