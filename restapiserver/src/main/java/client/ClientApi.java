f4d053a1c12d30917b4ebf555d4936c1b0f26ba9
Sangmin Lee <nalsm98@naver.com>
2020-09-05 00:03:05 +0900

    학습 모델 테스트 요청 API 호출 구현
    * 테스트 API 호출하는 로직 구현

--- restapiserver/src/main/java/client/ClientApi.java
+++ restapiserver/src/main/java/client/ClientApi.java
@@ -1,14 +1,22 @@
 package client;
 
 import dto.PostLearningToFlaskDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import dto.PostTestResponseByFlask;
+import dto.PostTestToFlaskDto;
 import org.springframework.web.client.RestTemplate;
 
 public class ClientApi {
 
+  private final String LEARNING_SERVER_URL = "http://127.0.0.1:5000";
+
   public void postLearningToFlask(PostLearningToFlaskDto dto) {
     RestTemplate restTemplate = new RestTemplate();
-    restTemplate.postForObject("http://127.0.0.1:5000/learning", dto, PostLearningToFlaskDto.class);
+    restTemplate.postForObject(LEARNING_SERVER_URL + "/learning", dto, PostLearningToFlaskDto.class);
+  }
+
+  public PostTestResponseByFlask postTestToFlask(PostTestToFlaskDto dto) {
+    RestTemplate restTemplate = new RestTemplate();
+    return restTemplate.postForObject(LEARNING_SERVER_URL + "/test", dto, PostTestResponseByFlask.class);
   }
 
 }
