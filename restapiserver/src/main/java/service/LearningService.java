7e730a4406016aa8104d12d1b0349741086e4b93
Sangmin Lee <nalsm98@naver.com>
2020-09-04 23:57:21 +0900

    LearningService update
    * 학습 정보를 DB에 저장할 때, 입력 컬럼들과 출력 컬럼들 정보들도 저장하도록 로직 수정

--- restapiserver/src/main/java/service/LearningService.java
+++ restapiserver/src/main/java/service/LearningService.java
@@ -1,13 +1,19 @@
 package service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.LearningDto;
 import dto.MemberDto;
+import dto.PostLearningRequestDto;
 import dto.ResultByInsertLearningDto;
 import error.MemberNotFoundException;
 import mapper.LearningMapper;
 import mapper.MemberMapper;
+import org.postgresql.util.PGobject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 @Service
 public class LearningService {
@@ -17,10 +23,18 @@ public class LearningService {
   @Autowired
   private MemberMapper memberMapper;
 
-  public ResultByInsertLearningDto insert(LearningDto learningDto) {
-    if(memberMapper.selectOneById(learningDto.getMemberId()) == null)
+  public ResultByInsertLearningDto insert(PostLearningRequestDto dto) throws JsonProcessingException, SQLException {
+    if(memberMapper.selectOneById(dto.getMemberId()) == null)
       throw new MemberNotFoundException();
+    LearningDto learningDto = new LearningDto(dto);
+    PGobject input = new PGobject();
+    input.setType("json");
+    input.setValue(new ObjectMapper().writeValueAsString(dto.getInputColumns()));
+    PGobject output = new PGobject();
+    output.setType("json");
+    output.setValue(new ObjectMapper().writeValueAsString(dto.getOutputColumns()));
     learningMapper.insert(learningDto);
+    learningMapper.insertColumns(input, output, learningDto.getId());
     return new ResultByInsertLearningDto(learningDto.getId());
   }
 
