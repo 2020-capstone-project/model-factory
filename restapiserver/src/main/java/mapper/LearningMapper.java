85bf3d516ca78886b1df5c41540f9d9550cd380f
Sangmin Lee <nalsm98@naver.com>
2020-09-05 00:00:18 +0900

    LearningMapper update
    * 입력 컬럼들과 출력 컬럼들을 저장하는 로직 추가

--- restapiserver/src/main/java/mapper/LearningMapper.java
+++ restapiserver/src/main/java/mapper/LearningMapper.java
@@ -2,6 +2,7 @@ package mapper;
 
 import dto.LearningDto;
 import org.apache.ibatis.annotations.*;
+import org.postgresql.util.PGobject;
 
 public interface LearningMapper {
 
@@ -9,5 +10,12 @@ public interface LearningMapper {
   @Insert("insert into learning (batchSize, epoch, lossFunction, optimizerFunction, learningDate, memberId, name) " +
       "values (#{batchSize}, #{epoch}, #{lossFunction}, #{optimizerFunction}, now(), #{memberId}, #{name})")
   void insert(LearningDto learningDto);
+
+  @Update("update learning " +
+      "set inputcolumns = #{inputColumns}, outputcolumns = #{outputColumns} " +
+      "where id = #{id}")
+  void insertColumns(@Param("inputColumns") PGobject inputColumns,
+                     @Param("outputColumns") PGobject outputColumns,
+                     @Param("id") int id);
 
 }
