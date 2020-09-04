247ed9984f862f4096c6abf5aea84cea022d1ac2
Sangmin Lee <nalsm98@naver.com>
2020-09-05 00:00:56 +0900

    ModelMapper update
    * diagramPath 를 제거
    * 학습 번호를 통해 모델을 조회하는 로직 추가

--- restapiserver/src/main/java/mapper/ModelMapper.java
+++ restapiserver/src/main/java/mapper/ModelMapper.java
@@ -4,12 +4,17 @@ import dto.LearningDto;
 import dto.ModelDto;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface ModelMapper {
 
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  @Insert("insert into model (modelPath, diagramPath, createdDate, learningId) values " +
-      "(#{modelPath}, #{diagramPath}, now(), #{learningId})")
+  @Insert("insert into model (modelPath, createdDate, learningId) values " +
+      "(#{modelPath}, now(), #{learningId})")
   void insert(ModelDto ModelDto);
+
+  @Select("select modelPath from model where learningId = #{id}")
+  ModelDto selectOneByLearningId(@Param("id") int learningId);
 
 }
